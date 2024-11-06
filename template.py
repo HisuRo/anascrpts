a2abcddef1feee80751993507ab76a993b4098ae
+Tatsuhiro Nasu <nasut@omega-b.gat.com>
2024-11-06 11:45:45 -0800

    made template scripts for any input and pickle input

---
+++ template.py
@@ -0,0 +1,41 @@
+from nasu import get_d3d, system
+import matplotlib.pyplot as plt # type: ignore
+
+# initial setting and input
+config, wd = system.check_working_directory()
+input_filepath, tmpdir, outdir_base = system.define_input_tmp_output_directories(wd, config)
+inputs, outdir = system.load_input(input_filepath, outdir_base)
+now, logs = system.get_logs(wd)
+
+### input file template ### EDIT HERE !!
+""" 
+{
+	"outdirname" : "120329_spectra", 
+	"output_filename": "mmspc4_120329_spectra", 
+	"" : ""
+}
+"""
+#############
+
+# main # EDIT HERE !!
+
+# plot # EDIT HERE !!
+fig, ax = plt.subplots()
+ax.plot()
+ax.set_xlabel("")
+ax.set_ylabel("")
+ax.set_xscale("")
+ax.set_yscale("")
+ax.legend()
+fig.suptitle(f"")
+fig.tight_layout()
+
+# output # EDIT HERE !!
+outputs = {
+	'fig': fig
+}
+
+# systematic output and close
+output_filepath = system.output_pickle_file(outputs, inputs, logs, outdir)
+system.output_fig(fig, outdir, inputs, output_filepath, now)
+print("DONE !!")
---
+++ template_pickleinput.py
@@ -0,0 +1,46 @@
+# mask and linear fit to spectra, calculating powerlaw
+
+import numpy as np # type: ignore
+import matplotlib.pyplot as plt # type: ignore
+from nasu import system
+
+# initial setting and input
+config, wd = system.check_working_directory()
+input_filepath, tmpdir, outdir_base = system.define_input_tmp_output_directories(wd, config)
+inputs, outdir = system.load_input(input_filepath, outdir_base)
+now, logs = system.get_logs(wd)
+data = system.load_pickle_data(inputs)
+
+### input ### EDIT HERE !!!
+""" template 
+{
+	"outdirname" : "120329_spectra", 
+	"output_filename" : "mmspc4_120329_spectra_powerlaw",  
+	"input_datpath" : "/fusion/projects/xpsi/turbulence_and_transport/nasut/120329_spectra/mmspc4_120329_spectra.pkl", 
+	"" : 
+}
+"""
+#############
+
+# main EDIT HERE !!
+
+# plot EDIT HERE !!
+fig, ax = plt.subplots()
+ax.plot()
+ax.set_xscale("")
+ax.set_yscale("")
+ax.set_xlabel("")
+ax.set_ylabel("")
+ax.legend()
+fig.suptitle("")
+fig.tight_layout()
+
+# output EDIT HERE !!
+outputs = {
+	"fig": fig
+}
+
+# systematic output and close
+output_filepath = system.output_pickle_file(outputs, inputs, logs, outdir)
+system.output_fig(fig, outdir, inputs, output_filepath, now)
+print("DONE !!")

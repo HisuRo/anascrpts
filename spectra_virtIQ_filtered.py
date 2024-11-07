4de884a91304d988a6b4f8483348b34cb31894bc
+Tatsuhiro Nasu <nasut@omega-b.gat.com>
2024-11-06 17:15:26 -0800

    bug fixed

--- spectra_virtIQ_filtered.py
+++ spectra_virtIQ_filtered.py
@@ -1,5 +1,6 @@
 from nasu import get_d3d, system, calc
 import matplotlib.pyplot as plt # type: ignore
+import numpy as np # type: ignore
 
 # initial setting and input
 config, wd = system.check_working_directory()
@@ -39,8 +40,8 @@ now, logs = system.get_logs(wd)
 lowk = get_d3d.timetrace_multidomains(inputs['pointname'], inputs["shot"], inputs["idx_startdomain"], inputs["N_domain"])
 lowk.produce_virtual_IQ_signal(carrier_freq_Hz=inputs["carrier_freq"])
 
-lowk_filt = calc.notch(lowk.iq, lowk.Fs, inputs['f_notch'], inputs['Q_notch'])
-lowk_filt = calc.bandPass(lowk_filt, lowk.Fs, fp=[inputs["fpass_low"], inputs["fpass_high"]], fs=[inputs["fstop_low"], inputs["fstop_high"]])
+lowk_filt = calc.bandPass(lowk.iq, lowk.Fs, fp=[inputs["fpass_low"], inputs["fpass_high"]], fs=[inputs["fstop_low"], inputs["fstop_high"]])
+lowk_filt = calc.notch(lowk_filt, lowk.Fs, inputs['f_notch'], inputs['Q_notch'])
 
 sp1 = calc.spectrum(lowk.t_s, lowk_filt, lowk.Fs, inputs["tstart1"], inputs["tend1"], NFFT=2**14)
 sp2 = calc.spectrum(lowk.t_s, lowk_filt, lowk.Fs, inputs["tstart2"], inputs["tend2"], NFFT=2**14)
@@ -55,14 +56,12 @@ ax.plot(sp3.f, sp3.psd, label=f"{inputs['tstart3']} - {inputs['tend3']} s")
 ax.plot(sp4.f, sp4.psd, label=f"{inputs['tstart4']} - {inputs['tend4']} s")
 ax.set_xlabel("Frequency [Hz]")
 ax.set_ylabel("PSD filtered [V^2/Hz]")
-ax.set_xscale("log")
-ax.set_yscale("log")
 ax.legend()
 fig.suptitle(f"{inputs['pointname']} {inputs['shot']}")
 fig.tight_layout()
 
 fig2, ax2 = plt.subplots()
-ax2.plot(lowk.t_s, lowk_filt)
+ax2.plot(lowk.t_s, np.real(lowk_filt))
 ax2.set_xlabel("Time [s]")
 ax2.set_ylabel("Obsrerved electric field filtered [V]")
 fig2.suptitle(f"{inputs['pointname']} {inputs['shot']}")

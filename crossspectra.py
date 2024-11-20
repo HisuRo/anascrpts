452c0fe9a7e501df9b8a392b01e05bd634a6d446
+Tatsuhiro Nasu <nasut@omega-b.gat.com>
2024-11-20 13:21:46 -0800

    modify crossspectra.py to enable downsampling, multiple plots not only 4

--- crossspectra.py
+++ crossspectra.py
@@ -19,53 +19,56 @@ data2 = system.load_pickle_data(inputs, "input_datpath2")
 	"output_filename" : "mmspc4_mmspc3_120329_crossspectra_amplitudes_of_filtered",  
 	"input_datpath1" : "/fusion/projects/xpsi/turbulence_and_transport/nasut/120329_spectra/mmspc4_120329_spectra_amplitude_of_filtered.pkl", 
 	"input_datpath2" : "/fusion/projects/xpsi/turbulence_and_transport/nasut/120329_spectra/mmspc3_120329_spectra_amplitude_of_filtered.pkl", 
-	"tstart1" : 2.0, 
-	"tend1" : 2.3, 
-	"tstart2" : 2.3, 
-	"tend2" : 2.6, 
-	"tstart3" : 2.6, 
-	"tend3" : 2.9, 
-	"tstart4" : 2.9, 
-	"tend4" : 3.2, 
-	"NFFT" : 131072
+	"downsampling_factor" : 100, 
+	"tstart_list" : [2.0, 2.3, 2.6, 2.9], 
+	"tend_list" : [2.3, 2.6, 2.9, 3.2], 
+	"NFFT" : 1024
 }
 """
 #############
 
 # main EDIT HERE !!
 check.same_data(data1["t"], data2["t"])
-tw = get_d3d.twin_signals(data1["t"], data1["d"], data2["d"], data1["Fs"])
-cs1 = tw.cross_spectrum(inputs["tstart1"], inputs["tend1"], NFFT=inputs["NFFT"], ovr=0)
-cs2 = tw.cross_spectrum(inputs["tstart2"], inputs["tend2"], NFFT=inputs["NFFT"], ovr=0)
-cs3 = tw.cross_spectrum(inputs["tstart3"], inputs["tend3"], NFFT=inputs["NFFT"], ovr=0)
-cs4 = tw.cross_spectrum(inputs["tstart4"], inputs["tend4"], NFFT=inputs["NFFT"], ovr=0)
-noiselevel = 1. / cs1.NEns
+sig1 = get_d3d.signal(data1["t"], data1["d"], data1["Fs"])
+sig2 = get_d3d.signal(data2["t"], data2["d"], data2["Fs"])
+sig1.decimate(downsampling_factor=inputs["downsampling_factor"])
+sig2.decimate(downsampling_factor=inputs["downsampling_factor"])
 
-# plot EDIT HERE !!
+tw = get_d3d.twin_signals(sig1.dec.t_s, sig1.dec.d, sig2.dec.d, sig1.dec.Fs)
+
 fig, axs = plt.subplots(3, sharex=True)
 ax1, ax2, ax3 = axs
-ax1.plot(cs1.f, cs1.cohsq, label=f"{inputs['tstart1']} - {inputs['tend1']} s")
-ax1.plot(cs2.f, cs2.cohsq, label=f"{inputs['tstart2']} - {inputs['tend2']} s")
-ax1.plot(cs3.f, cs3.cohsq, label=f"{inputs['tstart3']} - {inputs['tend3']} s")
-ax1.plot(cs4.f, cs4.cohsq, label=f"{inputs['tstart4']} - {inputs['tend4']} s")
-ax1.hlines(noiselevel, cs1.f.min(), cs1.f.max(), ls="--", colors="grey")
-# ax.set_yscale("")
+
+Nsp = len(inputs["tstart_list"])
+cohsq_list = [0] * Nsp
+phase_list = [0] * Nsp
+csdamp_list = [0] * Nsp
+
+for i in range(Nsp):
+	tstart = inputs["tstart_list"][i]
+	tend = inputs["tend_list"][i]
+	cs = tw.cross_spectrum(tstart, tend, NFFT=inputs["NFFT"], ovr=0.5)
+	noiselevel = 4. / cs.NEns
+
+	cohsq_list[i] = cs.cohsq
+	phase_list[i] = cs.phase
+	csdamp_list[i] = cs.csdamp
+
+	ax1.plot(cs.f, cs.cohsq, label=f"{tstart} - {tend} s")
+	ax1.hlines(noiselevel, cs.f.min(), cs.f.max(), ls="--", colors="grey")
+	ax2.plot(cs.f, cs.phase, label=f"{tstart} - {tend} s")
+	ax3.plot(cs.f, cs.csdamp, label=f"{tstart} - {tend} s")
+
 ax1.set_ylabel("Coherence^2")
-ax1.set_ylim(0, noiselevel * 10)
+ax1.set_ylim(0, noiselevel * 5)
 ax1.legend()
 
-ax2.plot(cs1.f, cs1.phase, label=f"{inputs['tstart1']} - {inputs['tend1']} s")
-ax2.plot(cs2.f, cs2.phase, label=f"{inputs['tstart2']} - {inputs['tend2']} s")
-ax2.plot(cs3.f, cs3.phase, label=f"{inputs['tstart3']} - {inputs['tend3']} s")
-ax2.plot(cs4.f, cs4.phase, label=f"{inputs['tstart4']} - {inputs['tend4']} s")
 ax2.set_ylabel("Phase [rad]")
 
-ax3.plot(cs1.f, cs1.psd, label=f"{inputs['tstart1']} - {inputs['tend1']} s")
-ax3.plot(cs2.f, cs2.psd, label=f"{inputs['tstart2']} - {inputs['tend2']} s")
-ax3.plot(cs3.f, cs3.psd, label=f"{inputs['tstart3']} - {inputs['tend3']} s")
-ax3.plot(cs4.f, cs4.psd, label=f"{inputs['tstart4']} - {inputs['tend4']} s")
+ax3.set_xscale("log")
 ax3.set_xlabel("Frequency [Hz]")
 ax3.set_ylabel("ABS(CSD) [V^2/Hz]")
+ax3.set_yscale("log")
 
 fig.suptitle(f"{inputs['output_filename']}")
 fig.tight_layout()
@@ -73,22 +76,10 @@ fig.tight_layout()
 # output EDIT HERE !!
 outputs = {
 	"fig": fig, 
-	"f1" : cs1.f, 
-	"cohsq1" : cs1.cohsq, 
-	"phase1" : cs1.phase, 
-	"csdamp1" : cs1.psd, 
-	"f2" : cs2.f, 
-	"cohsq2" : cs2.cohsq, 
-	"phase2" : cs2.phase,
-	"csdamp2" : cs1.psd,  
-	"f3" : cs3.f, 
-	"cohsq3" : cs3.cohsq, 
-	"phase3" : cs3.phase, 
-	"csdamp2" : cs1.psd, 
-	"f4" : cs4.f, 
-	"cohsq4" : cs4.cohsq, 
-	"phase4" : cs4.phase, 
-	"csdamp4" : cs1.psd
+	"f" : cs.f, 
+	"cohsq" : cohsq_list, 
+	"phase" : phase_list, 
+	"csdamp" : csdamp_list, 
 }
 
 # systematic output and close

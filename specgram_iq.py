feb9a7f227c5040b9818f67c55600e66829a16bc
HisuRo <nasu.tatsuhiro@nifs.ac.jp>
2024-12-16 12:00:03 +0900

    created specgram.py and change the name of old specgram.py to specgram_iq.py

--- specgram_iq.py
+++ specgram_iq.py
@@ -18,14 +18,15 @@ now, logs = system.get_logs(wd)
     "tstart" : 2.5, 
     "tend" : 8.5, 
     "diagname" : "MWRM-PXI", 
-    "ch" : 1, 
+    "ch_i" : 1, 
+    "ch_q" : 2, 
     "NFFT" : 1024
 }
 """
 #############
 
 # main # EDIT HERE !!
-tt = get_labcom.timetrace(sn=inputs["sn"], subsn=inputs["subsn"], tstart=inputs["tstart"], tend=inputs["tend"], diagname=inputs["diagname"], ch=inputs["ch"])
+tt = get_labcom.timetrace_iq(sn=inputs["sn"], subsn=inputs["subsn"], tstart=inputs["tstart"], tend=inputs["tend"], diagname=inputs["diagname"], ch_i=inputs["ch_i"], ch_q=inputs["ch_q"])
 tt.raw.specgram(NFFT=inputs["NFFT"])
 
 # plot # EDIT HERE !!
@@ -35,7 +36,7 @@ cbar = fig.colorbar(pcm, ax=ax)
 cbar.set_label("PSD [V$^2$/Hz]")
 ax.set_xlabel("Time [s]")
 ax.set_ylabel("Frequency [Hz]")
-fig.suptitle(f"{inputs['diagname']} {inputs['ch']}\n"
+fig.suptitle(f"{inputs['diagname']} {inputs['ch_i']} {inputs['ch_q']}\n"
 			f"{inputs['sn']}-{inputs['subsn']}")
 fig.tight_layout()
 
